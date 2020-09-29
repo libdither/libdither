485a2ee48745dd92ab9998ae733e68e54f310868
Zyansheep <zyansheep@gmail.com>
2020-09-29 13:31:53 -0400

    base lib structure

--- research/cmd/src/main.rs
+++ research/cmd/src/main.rs
@@ -1,3 +1,22 @@
+
+
 fn main() {
-    println!("Hello, world!");
+	let client = IpfsClient::default();
+	env_logger::Builder::new().filter_level(log::LevelFilter::Info).init(); // Init Logger
+	
+	let yaml = load_yaml!("app.yml");
+	let app = App::from_yaml(yaml);
+	let matches = app.get_matches();
+	match matches.args() {
+		
+	}
+	
+	match matches.subcommand() {
+		("peers", Some(sub_cmd)) => {
+			println!("Peers:");
+		},
+		("friends", Some(sub_cmd)) => {
+			println!("Friends:");
+		},
+	}
 }
--- research/core/src/lib.rs
+++ research/core/src/lib.rs
@@ -4,29 +4,35 @@ pub struct Config {
 	thing: bool,
 }
 pub struct User {
-	keypair: 
+	
 }
 
-pub struct Node {
+pub struct Client {
 	//Node data here
 	config: Config,
-	user: User,
+	accounts: ,
+}
+impl Client {
+	pub fn new(config: Config) {
+		let tcp = TcpConfig::new();
+		
+		// Generate PeerID keypair
+		let id_keys = Keypair::generate_ed25519();
+		
+		//noise_keys
+		let noise_keys = noise::Keypair::<noise::X25519Spec>::new().into_authentic(&id_keys).unwrap();
+		let noise = noise::NoiseConfig::xx(noise_keys).into_authenticated();
+		
+		let yamux = yamux::Config::default();
+		let transport = tcp.upgrade(upgrade::Version::V1).authenticate(noise).multiplex(yamux);
+		
+		let tcp = TcpConfig::new();
+		let addr: Multiaddr = "/unix//home/zyansheep/Dissonance/test1.socket".parse().expect("invalid multiaddr");
+		let _conn = tcp.dial(addr).await;
+	}
+	fn init() {
+		
+	}
 }
 
-fn init() {
-	let tcp = TcpConfig::new();
-	
-	// Generate PeerID keypair
-	let id_keys = Keypair::generate_ed25519();
-	
-	//noise_keys
-	let noise_keys = noise::Keypair::<noise::X25519Spec>::new().into_authentic(&id_keys).unwrap();
-	let noise = noise::NoiseConfig::xx(noise_keys).into_authenticated();
-	
-	let yamux = yamux::Config::default();
-	let transport = tcp.upgrade(upgrade::Version::V1).authenticate(noise).multiplex(yamux);
-	
-	let tcp = TcpConfig::new();
-	let addr: Multiaddr = "/unix//home/zyansheep/Dissonance/test1.socket".parse().expect("invalid multiaddr");
-	let _conn = tcp.dial(addr).await;
-}
+
