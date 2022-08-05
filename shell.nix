0c26900d0d444532e8da174ebe4db187664cb1f4
zyansheep <zyansheep@protonmail.com>
2022-08-05 15:28:45 -0400

    rename various things

--- shell.nix
+++ shell.nix
@@ -1,34 +1,7 @@
-
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-	buildInputs = with pkgs; [
-		# Build Tools
-		cargo
-		cmake
-		pkg-config
-		
-		# Xserver
-		x11
-		xorg.libX11.dev
-		xorg.libX11
-		xorg.libX11.dev.out
-		xorg.libXcursor
-		
-		# Protobuf
-		protobuf
-		python3
-	];
-	APPEND_LIBRARY_PATH = with pkgs; pkgs.stdenv.lib.makeLibraryPath [
-		vulkan-loader
-		xlibs.libXcursor
-		xlibs.libXi
-		xlibs.libXrandr
-	];
-
-	shellHook = ''
-		export PROTOC=$(which protoc)
-		export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
-		export RUSTFLAGS="-C target-cpu=native"
-	'';
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).shellNix
