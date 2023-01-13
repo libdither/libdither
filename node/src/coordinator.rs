ea600ebdeac5b2478fde5b263850fce68e31ed35
zyansheep <zyansheep@protonmail.com>
2023-01-13 15:14:16 -0500

    annotate adaptive coordinate algorithm

--- node/src/coordinator.rs
+++ node/src/coordinator.rs
@@ -78,26 +78,26 @@ impl<Coord: CoordinateType, const ADAPT_CONST: f64, const ERROR_CONST: f64> Node
             // Algorithm as found on page for in https://pdos.csail.mit.edu/papers/vivaldi:sigcomm/paper.pdf
 
             // Coordinate space relative displacement of the remote node
-            let mut displacement = self.coord.clone().subtract(remote.coord);
+            let mut displacement = self.coord.clone().subtract(remote.coord); // x_i - x_j
             
             // Estimate of distance of the remote node
-            let distance_estimate = displacement.length();
+            let distance_estimate = displacement.length(); // ||x_i - x_j||
 
             // Error is the absolute difference between the measured and estimated distances to the remote node.
-            let sample_error = remote.measured_distance.abs_sub(distance_estimate);
-            let relative_sample_error = sample_error / remote.measured_distance;
+            let sample_error = remote.measured_distance.abs_sub(distance_estimate); // 
+            let relative_sample_error = sample_error / remote.measured_distance; // e_s = | ||x_j - x_j|| - rtt | / rtt
 
             // Relative direction of the remote node
-            let dir = displacement.normalized();
+            let dir = displacement.normalized(); // u(x_i - x_j)
 
             // The higher our error is, the more remotes with lower errors effect our coordinates.
-            let sample_error_weight = self.error / (self.error + remote.error);
+            let sample_error_weight = self.error / (self.error + remote.error); // e_i / (e_i + e_j)
 
             // Update moving average of this node's error
-            self.error = relative_sample_error * ERROR_CONST * sample_error_weight + self.error * (1 - ERROR_CONST * sample_error_weight);
+            self.error = relative_sample_error * ERROR_CONST * sample_error_weight + self.error * (1 - ERROR_CONST * sample_error_weight); // e_i = ...
 
             // Timestep scales with error
-            let delta = ADAPT_CONST * sample_error_weight;
+            let delta = ADAPT_CONST * sample_error_weight; // δ = c_e * w
             // Move coord
             self.coord.add(delta * dir * sample_distance_difference);
         }
