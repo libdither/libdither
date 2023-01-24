1e4ad2062e533bd7f7e61cb4f1245d794b7bc71f
zyansheep <zyansheep@protonmail.com>
2023-01-23 19:12:12 -0500

    update coordinator

--- node/src/coordinator.rs
+++ node/src/coordinator.rs
@@ -1,12 +1,12 @@
 //! This crate implements provides traits and implementations for algorithms assigning coordinates to nodes.
+//! Adapted from the Vivaldi Protocol: https://pdos.csail.mit.edu/papers/vivaldi:sigcomm/paper.pdf
 
-use std::ops::Div;
+use std::ops::{Sub};
 
-use num::{Num, Float};
-
+use num::{Float};
 
 /// Trait for a CoordinateSystem as defined in the Vivaldi paper
-pub trait CoordinateType: Clone {
+pub trait CoordinateType: Clone + Default {
     type Scalar: Float;
     fn subtract(self, other: &Self) -> Self;
     fn add(self, other: &Self) -> Self;
@@ -23,11 +23,11 @@ pub trait CoordinateType: Clone {
     }
 }
 /// Trait for a Coordinator.
-pub trait NodeCoordinator {
+pub trait NodeCoordinator: Default {
     type Coord: CoordinateType;
     type RemoteState;
     /// Update the system 
-    fn update(&mut self, remotes: impl Iterator<Item = &mut RemoteState>);
+    fn update(&mut self, remotes: impl Iterator<Item = &mut Self::RemoteState>);
 }
 
 /// Coordinate with format Distance-Distance-Height using f64 as a scalar
@@ -36,7 +36,7 @@ struct CoordDDHf64 {
     position: nalgebra::Vector2<f64>,
     height: f64,
 }
-impl CoordinateType for CoordDDH {
+impl CoordinateType for CoordDDHf64 {
     type Scalar = f64;
 
     fn subtract(&mut self, other: &Self) {
@@ -59,7 +59,8 @@ impl CoordinateType for CoordDDH {
     }
 }
 
-struct AdaptiveCoordinator<Coord: CoordinateType, const MOVE_CONST: f64 = 0.25, const ERROR_CONST: f64 > {
+#[derive(Debug, Default)]
+struct AdaptiveCoordinator<Coord: CoordinateType, const ERROR_CONST: f64, const MOVE_CONST: f64 = 0.25> {
     coord: Coord,
     error: f64,
 }
@@ -68,30 +69,30 @@ struct AdaptiveCoordinatorRemoteState<Coord: CoordinateType> {
     error: f64,
     measured_distance: f64,
 }
-impl<Coord: CoordinateType, const ADAPT_CONST: f64, const ERROR_CONST: f64> NodeCoordinator for AdaptiveCoordinator<Coord, ADAPT_DELTA_CONST, ERROR_CONST> {
+impl<Coord: CoordinateType, const ADAPT_CONST: f64, const ERROR_CONST: f64> NodeCoordinator for AdaptiveCoordinator<Coord, ERROR_CONST, ADAPT_CONST> {
     type Coord = Coord;
 
     type RemoteState = AdaptiveCoordinatorRemoteState<Coord>;
 
-    fn update(&mut self, remotes: impl Iterator<Item = &mut RemoteState>) {
+    fn update(&mut self, remotes: impl Iterator<Item = &mut Self::RemoteState>) {
         for remote in remotes {
-            // Algorithm as found on page for in https://pdos.csail.mit.edu/papers/vivaldi:sigcomm/paper.pdf
+            // Algorithm as found here: https://pdos.csail.mit.edu/papers/vivaldi:sigcomm/paper.pdf#page=4
 
             // Coordinate space relative displacement of the remote node
-            let mut displacement = self.coord.clone().subtract(remote.coord); // x_i - x_j
+            let mut displacement = self.coord.clone().subtract(&remote.coord); // x_i - x_j
             
             // Estimate of distance of the remote node
             let distance_estimate = displacement.length(); // ||x_i - x_j||
 
             // Error is the absolute difference between the measured and estimated distances to the remote node.
-            let sample_error = remote.measured_distance.abs_sub(distance_estimate); // 
-            let relative_sample_error = sample_error / remote.measured_distance; // e_s = | ||x_j - x_j|| - rtt | / rtt
+            let sample_error: f64 = remote.measured_distance.sub(distance_estimate); // rtt - ||x_j - x_j||
+            let relative_sample_error = sample_error.abs() / remote.measured_distance; // e_s = | ||x_j - x_j|| - rtt | / rtt
 
             // Relative direction of the remote node
             let dir = displacement.normalized(); // u(x_i - x_j)
 
             // The higher our error is, the more remotes with lower errors effect our coordinates.
-            let sample_error_weight = self.error / (self.error + remote.error); // e_i / (e_i + e_j)
+            let sample_error_weight = self.error / (self.error + remote.error); // w = e_i / (e_i + e_j)
 
             // Update moving average of this node's error
             self.error = relative_sample_error * ERROR_CONST * sample_error_weight + self.error * (1 - ERROR_CONST * sample_error_weight); // e_i = ...
@@ -99,7 +100,7 @@ impl<Coord: CoordinateType, const ADAPT_CONST: f64, const ERROR_CONST: f64> Node
             // Timestep scales with error
             let delta = ADAPT_CONST * sample_error_weight; // δ = c_e * w
             // Move coord
-            self.coord.add(delta * dir * sample_distance_difference);
+            self.coord.add((delta * sample_error) * dir);
         }
     }
 }
